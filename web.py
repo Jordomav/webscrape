94b9a73d4fc7b1c857e1ef816f877b39353c56ef
Jordan Mavrogeorge <jordomav@gmail.com>
2017-03-16 01:04:04 -0700

    parse the initial webpage then grab the first link on that page and scrape that web page

--- web.py
+++ web.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-file = requests.get("https://github.com/Jordomav/webscrape")
+file = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
 
 c = file.content
 
 soup = BeautifulSoup(c, 'html.parser')
 
-print(soup.prettify())
+# Get all links on the page of they are https
+def get_first_link():
+    for link in soup.find_all('a'):
+        if "https" in str(link):
+            return link.get('href')
+
+source = requests.get(get_first_link()).content
+
+py_soup = BeautifulSoup(source, 'html.parser')
+
+print(py_soup.prettify())
